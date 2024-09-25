bb54a9a85384e42978606d586a10652548918ebb
Guilherme dos Santos <gs92dev@gmail.com>
2024-09-24 22:37:11 -0500

    fetching data

---
+++ src/Gui.tsx
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+
+export function Gui() {
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["todo"], // Change "todo" if your API endpoint expects a different key
+    queryFn: () =>
+      fetch("https://catfact.ninja/fact").then((res) => res.json()),
+  });
+
+  if (error) {
+    console.error(error);
+    return <p>Something went wrong</p>;
+  }
+
+  return (
+    <div className="p-20">
+      {isLoading ? (
+        <p>Loading cat fact...</p>
+      ) : (
+        <div key={data.fact}>
+          {" "}
+          {/* Use data.fact as a unique key */}
+          <h1>{data.fact}</h1>
+          <h1>{data.length}</h1> {/* Check if data has a length property */}
+        </div>
+      )}
+    </div>
+  );
+}
--- src/routes/gui.tsx
+++ src/routes/gui.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { Gui } from "../Gui";
 
 export const Route = createFileRoute("/gui")({
-  component: () => <div className="text-red-500">Hello /gui!</div>,
+  component: () => <Gui />,
 });
